10298413ab6f11a8179cf526098663c9fbd6e044
HuangCher <165945110+HuangCher@users.noreply.github.com>
2024-11-12 12:21:03 -0500

    Added tabs and a temporary pet.

--- popup.js
+++ popup.js
@@ -1,4 +1,33 @@
+//switching between tabs
+function openTab(evt, tabName) {
+  const tabContent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  const tablinks = document.getElementsByClassName("tabs");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  const tabButtons = document.getElementsByClassName("tabs");
+  for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener("click", function(evt) {
+      const tabName = this.getAttribute("data-tab");
+      openTab(evt, tabName);
+    });
+  }
+
+  // pet tab is the default tab
+  document.getElementById("Pet").style.display = "block";
+  tabButtons[0].classList.add("active");
+
+  //stats tab
     chrome.storage.local.get(["trackedSites", "unproductiveTime"], (data) => {
       const trackedSitesDiv = document.getElementById("tracked-sites");
       const unproductiveTimeDiv = document.getElementById("unproductive-time");
